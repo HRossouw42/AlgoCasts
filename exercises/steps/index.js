7453d45fcf11d9f135d02822b366d779fcd2fa9f
Harmun <harmun.rossouw@coil.com>
2021-07-20 15:44:21 +0200

    recursive steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,55 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// recursive
+function steps(n, row = 0, stair = "") {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    steps(n, row + 1);
+    return;
+  }
+
+  if (stair.length <= row) {
+    stair += "#";
+  } else {
+    stair += " ";
+  }
+  steps(n, row, stair);
+}
 
 module.exports = steps;
+
+// // mine
+// function steps(n) {
+//   for (let i = 1; i <= n; i++) {
+//     let str = "";
+//     let pound = i;
+//     let spaces = n - i;
+//     for (let j = 0; j < pound; j++) {
+//       str += "#";
+//     }
+//     for (let k = 0; k < spaces; k++) {
+//       str += " ";
+//     }
+//     console.log(str);
+//   }
+// }
+
+// iterative
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = "";
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         stair += "#";
+//       } else {
+//         stair += " ";
+//       }
+//     }
+//     console.log(stair);
+//   }
+// }
