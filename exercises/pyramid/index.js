2457547f6caa5e6d03ab4f82e33f1b50e602663b
Harmun <harmun.rossouw@coil.com>
2021-07-26 16:27:43 +0200

    pyramid iterative

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,21 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  for (let row = 0; row < n; row++) {
+    let level = "";
+    for (let col = 0; col < 2 * n - 1; col++) {
+      // midpoint range is from midpoint - row to midpoint + row
+      // check is column index between the midpoint range
+      if (midpoint - row <= col && midpoint + row >= col) {
+        level += "#";
+      } else {
+        level += " ";
+      }
+    }
+    console.log(level);
+  }
+}
 
 module.exports = pyramid;
